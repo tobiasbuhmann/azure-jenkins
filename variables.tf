c67cbc8803fc63d8361848ae2712d26e354977c2
tobiasbuhmann <51085142+tobiasbuhmann@users.noreply.github.com>
2023-02-16 19:49:25 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable location {
+    type = string
+}
+
+variable subscriptionName {
+    type = string
+}
+
+variable region {
+    type = string
+}
+
+variable environment {
+    type = string
+}
+
+variable tags {
+    type = map(string)
+}
